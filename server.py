6b205d9565142d715636f8737955457014d74e65
fgonzalezberro <fgonzalezberro@gmail.com>
2020-02-07 12:50:33 -0300

    Python Local server Script

---
+++ server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Server en localhost:8080", PORT)
+    httpd.serve_forever()
